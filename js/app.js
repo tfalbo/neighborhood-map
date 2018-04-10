2dadc882948128ab36e32a55c3d40f64a5fb9bca
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-04-10 01:00:43 -0300

    refactor code to use knockout

--- js/app.js
+++ js/app.js
@@ -1,16 +1,64 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 20.91528, lng: -100.74389},
-    zoom: 17
-  });
+var locations = [
+    { 
+        'name': 'The Restaurant',
+        'lat':  20.913377,
+        'lng': -100.742783,
+    },
+    {
+        'name': 'Los Milagros',
+        'lat': 20.915380,
+        'lng':  -100.743010,
+    },
+    {
+        'name': 'La Azotea',
+        'lat':20.914314, 
+        'lng': -100.744346,
+    },
+    {
+        'name': 'La Sirena Gorda',
+        'lat': 20.914183, 
+        'lng': -100.745357,
+    },
+    {
+        'name': 'La Posadita',
+        'lat': 20.913242, 
+        'lng': -100.744181,
+    },
+];
 
-    for (var item in restaurants) {
-        restaurant = restaurants[item];
-        marker  = new google.maps.Marker({ 
-            position: { lat: parseFloat(restaurant['lat']), lng: parseFloat(restaurant['lng']) },
-            map: map,
-            title: restaurant['name'],  
-        });
-    }
+var map;
+
+var Location = function(data){
+
+    this.name = ko.observable(data.name);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
+
+    this.marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.lat, data.lng),
+        map: map,
+        title: data.name
+    });
+    
+    this.marker.setMap(map);
+}
+
+function AppViewModel(){
+    var self = this;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 20.91528, lng: -100.74389},
+        zoom: 17
+    });
+
+    this.locationList = ko.observableArray([]);
+
+    locations.forEach(function(locationItem){
+        self.locationList.push(new Location(locationItem));
+        console.log(self.locationList());
+    });
+}
+
+function initApp(){
+    ko.applyBindings(new AppViewModel());
 }
--- js/data.js
+++
@@ -1,6 +0,0 @@
-var restaurants = [
-    { 'name': 'The Restaurant',
-    'lat':  20.913377,
-    'lng': -100.742783,
-    }
-];
